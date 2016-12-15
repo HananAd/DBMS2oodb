d2eb5b1c5dcd78a469ed7d2f6f9068bc01b96474
HananAd <hanan_ads@hotmail.com>
2016-12-15 17:53:33 +0200

    Update create.sql

--- university/create.sql
+++ university/create.sql
@@ -112,7 +112,28 @@ END;
 --senior_lecturer
 
 --associate_lecturer
+create or replace type Associate_lecturer_t under Lecturer_t
+(
+no_honours number,
+year_join number,
 
+member procedure insert_associate_lecturer(
+new_no_honours in number,
+new_year_join in number,
+new_area in varchar2,
+new_pers_id in varchar2,
+new_pers_surname in varchar2,
+new_pers_fname in varchar2,
+new_pers_title in varchar2,
+new_pers_address in varchar2,
+new_pers_phone in varchar2,
+new_pers_postcode in number,
+new_campus_location in varchar2,
+new_bld_id in varchar2,
+new_off_no in varchar2
+),
+member procedure delete_associate_lecturer
+);
 --tutor
 
 --enrolls_in
